a598ffc66923136b0ee419ff91ba8524d718f845
Saleh Ahmad <salehahmad625@gmail.com>
2021-08-18 19:34:39 -0500

    adapted to shows

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-class Book {
-  constructor(title, author, pages, read) {
+class Show {
+  constructor(title, read) {
     this.title = title;
-    this.author = author;
-    this.pages = pages;
     this.read = read;
   }
   info() {
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    return `${this.title}, ${this.read}`;
   }
 }
 
 let myLibrary = [];
 
-function addBookToLibrary(Book) {
-  myLibrary.push(Book);
+function addShowToLibrary(show) {
+  myLibrary.push(show);
 }
 
-function removeBook(num) {
+function removeShow(num) {
   myLibrary.splice(num, 1);
   populateCards(myLibrary);
 }
@@ -36,27 +34,19 @@ function populateCards(arr) {
     title.setAttribute("class", "title");
     title.textContent = arr[i].title;
 
-    const author = document.createElement("div");
-    author.setAttribute("class", "author");
-    author.textContent = "By: " + arr[i].author;
-
-    const pages = document.createElement("div");
-    pages.setAttribute("class", "pages");
-    pages.textContent = arr[i].pages + " pages";
-
     const read = document.createElement("button");
     read.setAttribute("class", "card-button");
     read.setAttribute("id", "status");
     if (arr[i].read.toLowerCase() !== "finished") {
       read.classList.add("class", "unread");
-      read.textContent = "Still reading";
+      read.textContent = "Still watching";
     } else {
       read.textContent = "Finished";
     }
     read.addEventListener("click", () => {
       read.classList.toggle("unread");
       if (read.classList.contains("unread")) {
-        read.textContent = "Still reading";
+        read.textContent = "Still watching";
       } else {
         read.textContent = "Finished";
       }
@@ -66,13 +56,11 @@ function populateCards(arr) {
     remove.setAttribute("class", "card-remove");
     remove.textContent = "+";
     remove.addEventListener("click", () => {
-      removeBook(i);
+      removeShow(i);
     });
 
     card.appendChild(remove);
     card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
     card.appendChild(read);
   
 
@@ -80,9 +68,9 @@ function populateCards(arr) {
   }
 }
 
-const addBook = document.querySelector("#add");
+const addShow = document.querySelector("#add");
 
-addBook.addEventListener("click", () => {
+addShow.addEventListener("click", () => {
   document.querySelector(".bg-modal").style.display = "flex";
 });
 
@@ -96,16 +84,14 @@ const form = document.getElementById("myForm");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let title = document.getElementById("formTitle").value;
-  let author = document.getElementById("formAuthor").value;
-  let pages = document.getElementById("formPages").value;
   let finished = 0;
   if (document.getElementById("formFinished").checked) {
     finished = document.getElementById("formFinished").value;
   } else {
-    finished = document.getElementById("stillReading").value;
+    finished = document.getElementById("stillWatching").value;
   }
-  const bookie = new Book(title, author, pages, finished);
+  const newShow = new Show(title, finished);
   document.querySelector(".bg-modal").style.display = "none";
-  addBookToLibrary(bookie);
+  addShowToLibrary(newShow);
   populateCards(myLibrary);
 });
